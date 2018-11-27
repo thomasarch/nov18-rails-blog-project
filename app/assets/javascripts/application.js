889b8ddfd5bd3436c58f508f5bd9afb2d34f35b8
Thomas DeFelice <thomasarch@gmail.com>
2018-11-27 11:47:12 -0500

    add posts and comments sections

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,10 @@
 //
 //= require rails-ujs
 //= require activestorage
+
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap-sprockets
+
 //= require turbolinks
 //= require_tree .
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  resources :comments
+  resources :posts
+  get 'index' => 'practice#index'
+  post 'index' => 'practice#index'
+  get 'about' => 'practice#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+
