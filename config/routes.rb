72df304346784d5bbc052220ca5e76ffd7b1d8e6
Thomas DeFelice <thomasarch@gmail.com>
2018-11-28 08:47:02 -0500

    style page add time to posts and comments

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'index' => 'practice#index'
   post 'index' => 'practice#index'
   get 'about' => 'practice#about'
+  root 'posts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
 
